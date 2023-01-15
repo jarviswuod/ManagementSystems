9b2a94a8d2eb0bc05c8f486ec620619c98bfe574
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-01-15 23:07:36 +0300

    Third commit
    Added a couple of changes over the last couple of days

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Products(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -18,9 +25,13 @@ class Products(models.Model):
     )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, null=True,choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -29,7 +40,7 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivery', 'Delivery'),
     )
-    # customer=
-    # products=
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    products = models.ForeignKey(Products, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_orders': total_orders, 'delivered': delivered,
+               'pending': pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    product = Products.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': product})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    custom = Customer.objects.get(id=pk_test)
+
+    orders = custom.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': custom, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+    context = {}
+
+    return render(request, 'accounts/order_form.html', context)
