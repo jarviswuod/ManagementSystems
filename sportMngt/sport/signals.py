de30a96d3471e802d124403600074fb19cd0db82
jarvisthedev <johnsonmakings@gmail.com>
2024-07-09 22:39:40 +0300

    Signal testing

--- sportMngt/sport/signals.py
+++ sportMngt/sport/signals.py
@@ -6,6 +6,9 @@ from .models import Game, TeamSeason
 
 @receiver(post_save, sender=Game)
 def update_team_season_stats(sender, instance, **kwargs):
+    print(11111111)
+    print(22222222)
+    print(33333333)
     if instance.is_played:
         # Determine the outcome for each team
         home_win = instance.home_team_score > instance.away_team_score
@@ -36,6 +39,10 @@ def update_team_season_stats(sender, instance, **kwargs):
                 home_team_season.lost += 1
             home_team_season.save()
 
+            print(f'${home_team_season}')
+            print(f'${home_team_season}')
+            print(f'${home_team_season}')
+
         # Update the away team stats
         away_team_season, created = TeamSeason.objects.get_or_create(
             season_id=instance.season_id, team_id=instance.away_team_id,
@@ -59,3 +66,7 @@ def update_team_season_stats(sender, instance, **kwargs):
             else:
                 away_team_season.lost += 1
             away_team_season.save()
+
+            print(f'${away_team_season}')
+            print(f'${away_team_season}')
+            print(f'${away_team_season}')
