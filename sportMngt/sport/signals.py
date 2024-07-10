1cf52d9426963b2cbe247b62685f55b22a68df3f
jarvisthedev <johnsonmakings@gmail.com>
2024-07-10 13:08:46 +0300

    Game & TeamSeason correction

--- sportMngt/sport/signals.py
+++ sportMngt/sport/signals.py
@@ -6,67 +6,54 @@ from .models import Game, TeamSeason
 
 @receiver(post_save, sender=Game)
 def update_team_season_stats(sender, instance, **kwargs):
-    print(11111111)
-    print(22222222)
-    print(33333333)
     if instance.is_played:
-        # Determine the outcome for each team
+
         home_win = instance.home_team_score > instance.away_team_score
         away_win = instance.away_team_score > instance.home_team_score
         draw = instance.home_team_score == instance.away_team_score
 
-        # Update the home team stats
         home_team_season, created = TeamSeason.objects.get_or_create(
             season_id=instance.season_id, team_id=instance.home_team_id,
             defaults={'points': 0, 'wins': 0,
                       'draws': 0, 'lost': 0, 'games_played': 0}
         )
-        if not created:
-            home_team_season.games_played = Game.objects.filter(
-                Q(home_team_id=instance.home_team_id) | Q(
-                    away_team_id=instance.home_team_id),
-                season_id=instance.season_id,
-                is_played=True
-            ).count()
 
-            if home_win:
-                home_team_season.wins += 1
-                home_team_season.points += 3
-            elif draw:
-                home_team_season.draws += 1
-                home_team_season.points += 1
-            else:
-                home_team_season.lost += 1
-            home_team_season.save()
+        home_team_season.games_played = Game.objects.filter(
+            Q(home_team_id=instance.home_team_id) | Q(
+                away_team_id=instance.home_team_id),
+            season_id=instance.season_id,
+            is_played=True
+        ).count()
 
-            print(f'${home_team_season}')
-            print(f'${home_team_season}')
-            print(f'${home_team_season}')
+        if home_win:
+            home_team_season.wins += 1
+            home_team_season.points += 3
+        elif draw:
+            home_team_season.draws += 1
+            home_team_season.points += 1
+        else:
+            home_team_season.lost += 1
+        home_team_season.save()
 
-        # Update the away team stats
         away_team_season, created = TeamSeason.objects.get_or_create(
             season_id=instance.season_id, team_id=instance.away_team_id,
             defaults={'points': 0, 'wins': 0,
                       'draws': 0, 'lost': 0, 'games_played': 0}
         )
-        if not created:
-            away_team_season.games_played = Game.objects.filter(
-                Q(home_team_id=instance.away_team_id) | Q(
-                    away_team_id=instance.away_team_id),
-                season_id=instance.season_id,
-                is_played=True
-            ).count()
 
-            if away_win:
-                away_team_season.wins += 1
-                away_team_season.points += 3
-            elif draw:
-                away_team_season.draws += 1
-                away_team_season.points += 1
-            else:
-                away_team_season.lost += 1
-            away_team_season.save()
+        away_team_season.games_played = Game.objects.filter(
+            Q(home_team_id=instance.away_team_id) | Q(
+                away_team_id=instance.away_team_id),
+            season_id=instance.season_id,
+            is_played=True
+        ).count()
 
-            print(f'${away_team_season}')
-            print(f'${away_team_season}')
-            print(f'${away_team_season}')
+        if away_win:
+            away_team_season.wins += 1
+            away_team_season.points += 3
+        elif draw:
+            away_team_season.draws += 1
+            away_team_season.points += 1
+        else:
+            away_team_season.lost += 1
+        away_team_season.save()
